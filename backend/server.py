461e67aeb00eff029e8f3658746b2c1af4df835e
lucas <lucas.lvn97439@gmail.com>
2024-01-23 15:31:42 +0400

    add endpoint for file upload to api

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
 import hug
 from models import index
-from controller import authentification
+from controller import authentification, fileManager
 
 api = hug.API(__name__)
 
 api.extend(authentification, "/api/authentification")
+api.extend(fileManager, "/api/file")
