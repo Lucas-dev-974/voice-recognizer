eecfb070ce2186b1c2dfd79622e5b7957a4508b1
lucas <lucas.lvn97439@gmail.com>
2024-01-23 15:31:24 +0400

    add model

--- backend/models/files.py
+++ backend/models/files.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Integer, String, Sequence
 from config.connexion import Base
 
 
-class Modelia(Base):
-    __tablename__ = "modelias"
+class File(Base):
+    __tablename__ = "files"
     id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
-    model_name = Column(String(50), unique=True)
-    path = Column(String(500), unique=True)
+    filename = Column(String(50))
+    location = Column(String(50))
