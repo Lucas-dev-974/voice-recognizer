317f0a325f6dec62f0f7477188055e27be51a31f
lucas <lucas.lvn97439@gmail.com>
2024-01-23 13:49:47 +0400

    update login to return "me" & "token"

--- backend/controller/authentification.py
+++ backend/controller/authentification.py
@@ -1,14 +1,9 @@
 from middleware.jwt import authenticate, create_jwt
-from falcon import HTTP_401, HTTPError
+from falcon import HTTP_401
 import hug
 from models.user import User, getUser
 from config.connexion import session
 from middleware.hash import hash_password, verify_password
-
-
-@hug.get("/", requires=authenticate)
-def endpoint_protected(request):
-    return {"message": "Bienvenue dans l'endpoint protégé!"}
 
 
 @hug.post("/register")
@@ -27,8 +22,10 @@ def register(name, lastName, email, password):
 @hug.post("/")
 def login(email, password, response):
     user = session.query(User).filter_by(email=email).first()
+
     if not verify_password(input_password=password, stored_hash=user.password):
         response.status = HTTP_401
         return "Mot de passe ou email incorrecte"
 
-    return "Ceci est un endpoint public."
+    token = create_jwt({"id": user.id, "email": email})
+    return {"me": getUser(user), "token": token}
