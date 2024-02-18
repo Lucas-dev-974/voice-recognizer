f7c0d4ba45271c789d2841ef6b9fa60be90e5d7b
lucas <lucas.lvn97439@gmail.com>
2024-02-18 16:51:46 +0400

    add LabeledTextInput component

---
+++ frontend/src/components/labeled/TextInput.tsx
@@ -0,0 +1,27 @@
+import { TextInput } from "../Input/TextInput";
+
+import "./TextInput.css";
+
+interface LabeledTextInputProps {
+  onInput: () => void;
+  label: string;
+  defaultValue: string;
+  id: string;
+  placeholder?: string;
+}
+
+export function LabeledTextInput(props: LabeledTextInputProps) {
+  return (
+    <div class="labeled-text-input">
+      <label class="text-input-labed" for={props.id}>
+        {props.label}
+      </label>
+      <TextInput
+        id={props.id}
+        defaultValue={props.defaultValue}
+        onInput={props.onInput}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+}
