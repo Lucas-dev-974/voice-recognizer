33e43a865d372b395b61fcc28975c90e1e06b8b3
lucas <lucas.lvn97439@gmail.com>
2024-02-20 10:35:55 +0400

    file change folder

---
+++ frontend/src/views/authentification/auth.utils.ts
@@ -0,0 +1,79 @@
+import { createSignal } from "solid-js";
+import { User } from "../../model/User";
+import { addError, saveInLocal } from "../../app.utils";
+import { setUser } from "../../app.state";
+import { AuthenticationService } from "../../services/authentication.service";
+
+export type UserLoginForm = Pick<User, "email" | "password">;
+export type UserRegisterForm = Omit<User, "id" | "token"> & {
+  passwordConfirmation: string;
+};
+
+export const [userRegisterForm, setUserRegisterForm] =
+  createSignal<UserRegisterForm>({
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    lastName: "",
+    name: "",
+  });
+
+export const [userLoginForm, setUserLoginForm] = createSignal<UserLoginForm>({
+  email: "",
+  password: "",
+});
+
+export class AuthUtils {
+  static async valideRegisterForm() {
+    if (
+      userRegisterForm()?.email &&
+      userRegisterForm()?.name &&
+      userRegisterForm()?.lastName &&
+      userRegisterForm()?.password &&
+      userRegisterForm()?.passwordConfirmation
+    ) {
+      const samePasswordAsVerificationPassword =
+        userRegisterForm()?.password !=
+        userRegisterForm()?.passwordConfirmation;
+
+      if (samePasswordAsVerificationPassword) {
+        addError({ content: "Les mot de passe ne correspondent pas." });
+        return;
+      }
+
+      const user = await AuthenticationService.register(
+        userRegisterForm() as UserRegisterForm
+      );
+
+      if (!user) return;
+      saveInLocal("user", user);
+    } else
+      addError({ content: "Veuillez compléter le formulaire de connexion." });
+  }
+
+  static async valideLoginForm() {
+    if (userLoginForm()?.email && userLoginForm()?.password) {
+      const user = await AuthenticationService.login(
+        userLoginForm() as UserLoginForm
+      );
+
+      if (!user) return;
+      saveInLocal("user", user);
+    } else
+      addError({ content: "Veuillez compléter le formulaire de connexion." });
+  }
+
+  static updateUserRegisterForm(user: Partial<UserRegisterForm>) {
+    setUserRegisterForm((user_) => {
+      if (!user_) return user_;
+      return { ...user_, ...user };
+    });
+  }
+
+  static updateUserLoginForm(user: Partial<UserRegisterForm>) {
+    setUserLoginForm((user_) => {
+      if (!user_) return user_;
+      return { ...user_, ...user };
+    });
+  }
+}
