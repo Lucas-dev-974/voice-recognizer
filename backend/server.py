6044c44dd26132f4f7424fb8fc5d10c390c54d73
lucas <lucas.lvn97439@gmail.com>
2024-02-20 10:02:24 +0400

    update server

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,15 @@
 import hug
 from models import index
 from controller import authentification, fileManager
+from hug.middleware import CORSMiddleware
 import tensorflow
 
 api = hug.API(__name__)
-
 api.extend(authentification, "/api/authentification")
 api.extend(fileManager, "/api/file")
+
+# Configuring CORS middleware
+api.http.add_middleware(CORSMiddleware(api, allow_origins=["*"]))
+
+if __name__ == "__main__":
+    api.http.serve()
