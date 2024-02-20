9625a1ae7187a32b72bab563f98b440d98aede85
lucas <lucas.lvn97439@gmail.com>
2024-02-20 10:36:14 +0400

    increase authentication page

--- frontend/src/views/authentification/Authentication.tsx
+++ frontend/src/views/authentification/Authentication.tsx
@@ -1,14 +1,22 @@
-import { JSXElement, Match, Switch, createSignal } from "solid-js";
+import { Match, Switch, createSignal } from "solid-js";
 import "./authentication.css";
 import { DefaultButton } from "../../components/button/Button";
 import { Login } from "./auth-form/Login";
 import { Register } from "./auth-form/Register";
+import { AuthUtils } from "./auth.utils";
 
 const [onAuth, setOnAuth] = createSignal<"login" | "register">("login");
 
 export function Authentication() {
-  const [callback, setCallback] = createSignal<() => void>();
+  function toggleAuthCard() {
+    if (onAuth() == "register") setOnAuth("login");
+    else setOnAuth("register");
+  }
 
+  async function validateForm() {
+    if (onAuth() == "login") await AuthUtils.valideLoginForm();
+    else await AuthUtils.valideRegisterForm();
+  }
   return (
     <div class="authentification">
       <div class="rounded relative bg-slate-100 lg:w-1/2 w-96 top-[-100px] xl:px-20 lg:px-15 md:px-15 px-10">
@@ -17,33 +25,18 @@ export function Authentication() {
             <Login />
           </Match>
           <Match when={onAuth() == "register"}>
-            <Register setValidateCallback={setCallback} />
+            <Register />
           </Match>
         </Switch>
 
         <div class="flex justify-end gap-2 py-3">
-          <Switch>
-            <Match when={onAuth() == "login"}>
-              <DefaultButton
-                text="S'enregistrer"
-                onClick={() => setOnAuth("register")}
-              />
-              <ValidateButton validate={callback} />
-            </Match>
-            <Match when={onAuth() == "register"}>
-              <DefaultButton
-                text="Se connecter"
-                onClick={() => setOnAuth("login")}
-              />
-              <ValidateButton validate={callback} />
-            </Match>
-          </Switch>
+          <DefaultButton
+            text={onAuth() == "register" ? "Se connecter" : "S'enregistrer"}
+            onClick={toggleAuthCard}
+          />
+          <DefaultButton text="Valider" onClick={validateForm} />
         </div>
       </div>
     </div>
   );
-}
-
-function ValidateButton(props: { validate: () => void }): JSXElement {
-  return <DefaultButton text="Valider" onClick={props.validate} />;
 }
